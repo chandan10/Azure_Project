f2093d9f8b6e387d38d85f1ae8103dd17a466966
chandan10 <patil.chandan10@gmail.com>
2024-12-18 15:56:32 +0530

    yaml updated for pool

--- Project/conftest.py
+++ Project/conftest.py
@@ -5,8 +5,8 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager  # Optional, handles driver installation
 
 # Test Setup
-@pytest.fixture
-def driver(autouse=True):
+@pytest.fixture(autouse=True)
+def driver():
     # Setup Chrome driver (you can use Firefox or another browser too)
     options = Options()
     options.headless = False  # Optional: run the browser in the background without UI
